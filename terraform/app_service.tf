0029f55543b072b12c822c2ecec99753db2e983c
Antony Bailey <support@antonybailey.net>
2022-02-25 17:10:42 +0000

    Add terraform action (#3)
    Co-authored-by: Antony Bailey <antonybailey@Antonys-MacBook-Air.local>

--- terraform/app_service.tf
+++ terraform/app_service.tf
@@ -1,5 +1,5 @@
 resource "azurerm_app_service" "app_service" {
-  name                = "app-cloudponents-${var.environment}-${var.environment}-${var.az_region_abbr_map[var.az_region]}"
+  name                = "app-cloudponents-${var.environment}-${var.az_region_abbr_map[var.az_region]}"
   location            = azurerm_resource_group.resource_group.location
   resource_group_name = azurerm_resource_group.resource_group.name
   app_service_plan_id = azurerm_app_service_plan.app_service_plan.id
